b60d598990a5ee849d319204dbf5e7ac5c69764d
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2024-01-12 12:48:04 +0100

    updated limit plotting script

--- plotLimits.py
+++ plotLimits.py
@@ -1,17 +1,19 @@
 import ROOT
 import json
 import copy
+import sys
 from TwoDAlphabet.ext import CMS_lumi
 
 
 mass_points = [
+  (1000, 300), (1000, 600), (1000, 800),
   (1200, 300), (1200, 600), (1200, 800), (1200, 1000),
-  (1500, 300), (1500, 600), (1500, 800), (1500, 1000), (1500, 1300),
-  (2000, 300), (2000, 600), (2000, 900), (2000, 1100), (2000, 1300), (2000, 1600),
-  (2500, 300), (2500, 600), (2500, 800), (2500, 1000), (2500, 1300), (2500, 1600), (2500, 1800),
-  (3000, 300), (3000, 600), (3000, 800), (3000, 1000), (3000, 1300), (3000, 1600), (3000, 1800), (3000, 2600), (3000, 2800),
-  (3500, 300), (3500, 600), (3500, 700), (3500, 1100), (3500, 1300), (3500, 1600), (3500, 2000), (3500, 2500), (3500, 2800),
-  (4000, 300), (4000, 600), (4000, 800), (4000, 1000), (4000, 1300), (4000, 1600), (4000, 2000), (4000, 2200), (4000, 2500), (4000, 2800)
+  (1600, 300), (1600, 600), (1600, 800), (1600, 1000), (1600, 1200), (1600, 1400),
+  (2000, 300), (2000, 600), (2000, 800), (2000, 1000), (2000, 1200), (2000, 1600), (2000, 1800),
+  (2500, 300), (2500, 600), (2500, 800), (2500, 1000), (2500, 1200), (2500, 1600), (2500, 2000), (2500, 2200),
+  (3000, 300), (3000, 600), (3000, 800), (3000, 1000), (3000, 1200), (3000, 1600), (3000, 2000), (3000, 2500), (3000, 2800),
+  (3500, 300), (3500, 600), (3500, 800), (3500, 1000), (3500, 1200), (3500, 1600), (3500, 2000), (3500, 2500), (3500, 2800),
+  (4000, 300), (4000, 600), (4000, 800), (4000, 1000), (4000, 1200), (4000, 1600), (4000, 2000), (4000, 2500), (4000, 2800)
 ]
 
 
@@ -24,6 +26,7 @@ def makePlot(fit_area, year, config, polyOrder):
     gr_limit = copy.deepcopy(ROOT.TGraph2D())
     gr_limit.SetTitle(";m_{X} [GeV];m_{Y} [GeV];95% CL expected upper limit (" + selection + ") [fb]")
 
+    max_xs_limit = 0.
     n = 0
     for (mX, mY) in mass_points:
         sample = 'XToYHTo6B_MX-{0}_MY-{1}'.format(mX, mY)
@@ -32,7 +35,7 @@ def makePlot(fit_area, year, config, polyOrder):
 
         xsec = config[year][sample]["xsec"]
         #print("xsec = {0}".format(xsec))
-        
+
         file_path = '{0}/{1}_area_{2}/higgsCombineTest.AsymptoticLimits.mH120.root'.format(fit_area, polyOrder, sample)
 
         f = ROOT.TFile.Open(file_path)
@@ -42,21 +45,27 @@ def makePlot(fit_area, year, config, polyOrder):
             #print(e.quantileExpected, e.limit)
         t.GetEntry(2) # get median expected limit
         limit = t.limit
-        #print("limit = {0}".format(limit))
-        
+
         xs_limit = limit * xsec * 1000
-        print("xs_limit = {0}".format(xs_limit))
-        
+        if xs_limit == 0.:
+            print("WARNING: Limit calculation failed. xs_limit = {0}".format(xs_limit))
+        else:
+            print("xs_limit = {0}".format(xs_limit))
+
+        if xs_limit > max_xs_limit:
+            max_xs_limit = xs_limit
+
         gr_limit.SetPoint(n,mX,mY,xs_limit)
-        
+
         n += 1
 
+    print("\nMaximum xs_limit: {0}\n".format(max_xs_limit))
 
     c = ROOT.TCanvas("c", "",1000,900)
     c.cd()
 
     gr_limit.SetMinimum(0.1) # for now put by hand
-    gr_limit.SetMaximum(700) # for now put by hand
+    gr_limit.SetMaximum(1000) # for now put by hand
 
     gr_limit.Draw("cont4z")
 
@@ -67,7 +76,7 @@ def makePlot(fit_area, year, config, polyOrder):
 
     c.SetLogz()
 
-    c.SaveAs('2017_{0}_expected_limits_2D.pdf'.format(selection))
+    c.SaveAs('{0}_{1}_expected_limits_2D.pdf'.format(year, selection))
 
 
 if __name__ == '__main__':
@@ -99,10 +108,10 @@ if __name__ == '__main__':
     ROOT.gStyle.SetStatFont(42)
     ROOT.gROOT.ForceStyle()
 
-    year = '2017'
+    year = sys.argv[1]
 
     json_file = open("../H3PO/Analysis/xsecs.json")
     config = json.load(json_file)
-    
-    makePlot('2017_boosted_SR_pass_toy_multiSignal', year, config, 1)
-    makePlot('2017_semiboosted_SR_pass_toy_multiSignal', year, config, 2)
+
+    makePlot('{0}_boosted_SR_pass_toy_multiSignal'.format(year), year, config, 1)
+    makePlot('{0}_semiboosted_SR_pass_toy_multiSignal'.format(year), year, config, 2)
